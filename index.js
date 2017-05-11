f489f82a5a15f3e3d3f518b1470d4cb4d86ca72d
Joan Pedreño <joan.pedreno@fhios.es>
2017-05-11 13:06:18 +0200

    remove wrong file

--- index.js
+++
@@ -1,6 +0,0 @@
-console.debug("running server");
-var telegraf = require('telegraf');
-var client = new telegraf(process.env.BOT_TOKEN);
-client.command('start', (ctx) => ctx.reply('YO!'));
-client.command('url', (ctx) => ctx.reply('http://google.es'));
-client.startPolling();
