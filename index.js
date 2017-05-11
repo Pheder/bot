71df3710676e6cf15baae96820ecc1231c1f2a06
Joan <joan@debian>
2017-05-11 15:19:27 +0200

    just the webhook

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var app = express();
 var os = require('os');
+
 app.use(express.static('www'));
 
 app.get('/', function (req, res) {
   res.send('');
 });
 
-
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-var telegraf = require('telegraf');
+/*var telegraf = require('telegraf');
 var client = new telegraf(process.env.BOT_TOKEN);
+client.telegram.setWebhook('https://debian/www');
+client.startWebhook('/www', null, 8443)
+
 client.command('start', (ctx) => ctx.reply('YO!'));
 client.command('url', (ctx) => ctx.reply('http://www.'+os.hostname() + ':3000'));
-client.startPolling();
+client.startPolling();*/
